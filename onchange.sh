933eebc40debd15c4902e1d842ff702125350102
Gregory Vincic <g@7de.se>
2019-11-26 05:35:07 +0100

    Initial revision

---
+++ onchange.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+path=$1
+dir=$(dirname "$path")
+filename=$(basename "$path")
+extension="${filename##*.}"
+nameonly="${filename%.*}"
+
+case $extension in
+    go)
+	goimports -w $path
+	go vet
+	;;
+esac
