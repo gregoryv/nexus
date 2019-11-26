98a340cafff5a6727b59d29d362e6dcbe3b99c06
Gregory Vincic <g@7de.se>
2019-11-26 06:37:26 +0100

    Document print funcs

--- print.go
+++ print.go
@@ -6,8 +6,10 @@ import (
 	"os"
 )
 
+// Stdout is the default destination of print funcs.
 var Stdout io.Writer = os.Stdout
 
+// Print returns a fmt.Print wrapper using err for errors.
 func Print(err *error) func(...interface{}) int {
 	fprint := Fprint(err)
 	return func(args ...interface{}) int {
@@ -15,6 +17,7 @@ func Print(err *error) func(...interface{}) int {
 	}
 }
 
+// Fprint returns a fmt.Fprint wrapper using err for errors.
 func Fprint(err *error) func(io.Writer, ...interface{}) int {
 	return func(w io.Writer, args ...interface{}) int {
 		if err != nil {
@@ -26,6 +29,7 @@ func Fprint(err *error) func(io.Writer, ...interface{}) int {
 	}
 }
 
+// Printf returns a fmt.Printf wrapper using err for errors.
 func Printf(err *error) func(string, ...interface{}) int {
 	fprintf := Fprintf(err)
 	return func(format string, args ...interface{}) int {
@@ -33,6 +37,7 @@ func Printf(err *error) func(string, ...interface{}) int {
 	}
 }
 
+// Fprintf returns a fmt.Fprintf wrapper using err for errors.
 func Fprintf(err *error) func(io.Writer, string, ...interface{}) int {
 	return func(w io.Writer, format string, args ...interface{}) int {
 		if err != nil {
@@ -44,6 +49,7 @@ func Fprintf(err *error) func(io.Writer, string, ...interface{}) int {
 	}
 }
 
+// Println returns a fmt.Println wrapper using err for errors.
 func Println(err *error) func(...interface{}) int {
 	fprintln := Fprintln(err)
 	return func(args ...interface{}) int {
@@ -51,6 +57,7 @@ func Println(err *error) func(...interface{}) int {
 	}
 }
 
+// Fprintln returns a fmt.Println wrapper using err for errors.
 func Fprintln(err *error) func(io.Writer, ...interface{}) int {
 	return func(w io.Writer, args ...interface{}) int {
 		if err != nil {
