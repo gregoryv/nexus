fc4133ba9120a3b15bf4e07de1aec08dec7d2c12
Gregory Vincic <g@7de.se>
2020-12-04 16:04:33 +0100

    Use pointer receiver

--- failure.go
+++ failure.go
@@ -5,7 +5,7 @@ type Failure struct {
 }
 
 // Fail sets the given error unless already failed. An error can only be set once.
-func (me Failure) Fail(err error) error {
+func (me *Failure) Fail(err error) error {
 	if me.err != nil {
 		return me.err
 	}
@@ -14,7 +14,7 @@ func (me Failure) Fail(err error) error {
 }
 
 // Ok return true if no error is set.
-func (me Failure) Ok() bool { return me.err == nil }
+func (me *Failure) Ok() bool { return me.err == nil }
 
 // Error returns the failure error or nil if not set
-func (me Failure) Error() error { return me.err }
+func (me *Failure) Error() error { return me.err }
