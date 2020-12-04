11949c360f5fae30d482b91591ff46e3059474c3
Gregory Vincic <g@7de.se>
2020-12-04 15:42:46 +0100

    Add type Failure

---
+++ failure.go
@@ -0,0 +1,17 @@
+package nexus
+
+type Failure struct {
+	err error
+}
+
+// Fail sets the given error unless already failed. An error can only be set once.
+func (me Failure) Fail(err error) error {
+	if me.err != nil {
+		return me.err
+	}
+	me.err = err
+	return err
+}
+
+// Ok return true if no error is set.
+func (me Failure) Ok() bool { return me.err == nil }
